d03571420620392266fc19f1893a9a179fd0309a
Thijs van Loef <thijsvanloef@gmail.com>
2025-02-11 19:40:07 +0100

    cleanup code

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 """This module contains the web application for the Dumpert video downloader."""
 from flask import Flask, render_template, request, flash, send_file
-import download_dumpert_video
+from download_dumpert_video import verify_input, download_video
 import secrets
 
 
@@ -13,15 +13,15 @@ def index():
     """Renders the index page."""
     if request.method == 'POST' and request.form.get('url'):
         url = request.form['url']
-        if not url:
-            flash('URL is required!')
+        if not verify_input(url):
+            flash('Invalid Dumpert URL!')
         else:
-            if not download_dumpert_video.verify_input(url):
-                flash('Invalid Dumpert URL!')
-            else:
-                return send_file(download_dumpert_video.download_video(url), as_attachment=True)
+            try:
+                return send_file(download_video(url), as_attachment=True)
+            except Exception as e:
+                flash(f'Error downloading video: {str(e)}')
     return render_template('index.html')
-
+    
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8080, threads=4)
