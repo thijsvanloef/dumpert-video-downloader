8a376e4e61b2d3fe196d70f9ec6ed75948958b74
Thijs van Loef <thijsvanloef@gmail.com>
2025-02-12 14:35:53 +0100

    add robots and sitemap

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 """This module contains the web application for the Dumpert video downloader."""
-from flask import Flask, render_template, request, flash, send_file
-from download_dumpert_video import verify_input, download_video
+from flask import Flask, render_template, request, flash, send_file, Response
+from download_dumpert_video import download_video
 import secrets
 
 
@@ -18,7 +18,28 @@ def index():
         except Exception as e:
             flash(f'Error downloading video: {str(e)}')
     return render_template('index.html')
-    
+
+
+@app.route('/robots.txt')
+def robots_txt():
+    """Serves the robots.txt file."""
+    content = "User-agent: *\nAllow: /\n\nSitemap: https://dumpert.loef.dev/sitemap.xml"
+    return Response(content, mimetype='text/plain')
+
+
+@app.route('/sitemap.xml')
+def sitemap_xml():
+    """Serves the sitemap.xml file."""
+    content = """<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://dumpert.loef.dev/</loc>
+            <changefreq>weekly</changefreq>
+        </url>
+    </urlset>"""
+    return Response(content, mimetype='application/xml')
+
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=8080, threads=4)
