85c36ded7f05fff44a237aa0fbc07e670ca45344
Thijs van Loef <thijsvanloef@gmail.com>
2025-02-13 13:24:02 +0100

    formatting

--- web.py
+++ web.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, flash, send_file, Response
 from download_dumpert_video import download_video
 import secrets
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(32)
 
